877cbefda33fbc330221265a7b64c8eafa18c17f
Warleson Candido <warleson.candido@rarolabs.com.br>
2020-11-04 16:29:19 -0300

    arrumando pastas

---
+++ lib/src/controller/tutorial_tutorial.dart
@@ -0,0 +1,76 @@
+library tutorial;
+
+import 'package:flutter/material.dart';
+import 'package:tutorial/src/models/tutorial_itens.dart';
+import 'package:tutorial/src/painter/painter.dart';
+
+class Tutorial {
+  static showTutorial(
+      BuildContext context, List<TutorialItens> children) async {
+    int count = 0;
+    var size = MediaQuery.of(context).size;
+    OverlayState overlayState = Overlay.of(context);
+    List<OverlayEntry> entrys = [];
+    children.forEach((element) async {
+      var offset = _capturePositionWidget(element.globalKey);
+      var sizeWidget = _getSizeWidget(element.globalKey);
+      entrys.add(
+        OverlayEntry(
+          builder: (context) {
+            return Scaffold(
+              backgroundColor: Colors.transparent,
+              body: Stack(
+                children: [
+                  CustomPaint(
+                    size: size,
+                    painter: HolePainter(
+                        shapeFocus: element.shapeFocus,
+                        dx: offset.dx + (sizeWidget.width / 2),
+                        dy: offset.dy + (sizeWidget.height / 2),
+                        width: sizeWidget.width,
+                        height: sizeWidget.height),
+                  ),
+                  ...element.children,
+                  Align(
+                    alignment: element.alignmentWidgetNextFocus ??
+                        Alignment.bottomCenter,
+                    child: Padding(
+                      padding: const EdgeInsets.all(30.0),
+                      child: GestureDetector(
+                        child: element.clicknextFocusWidget ??
+                            Text(
+                              "NEXT",
+                              style: TextStyle(color: Colors.white),
+                            ),
+                        onTap: () {
+                          entrys[count].remove();
+                          count++;
+                          if (count != entrys.length) {
+                            overlayState.insert(entrys[count]);
+                          }
+                        },
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            );
+          },
+        ),
+      );
+    });
+
+    overlayState.insert(entrys[0]);
+  }
+
+  static Offset _capturePositionWidget(GlobalKey key) {
+    RenderBox renderPosition = key.currentContext.findRenderObject();
+
+    return renderPosition.localToGlobal(Offset.zero);
+  }
+
+  static Size _getSizeWidget(GlobalKey key) {
+    RenderBox renderSize = key.currentContext.findRenderObject();
+    return renderSize.size;
+  }
+}
--- lib/src/painter/painter.dart
+++ lib/src/painter/painter.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:tutorial/models/shape_models.dart';
+import 'package:tutorial/src/models/shape_models.dart';
 
 class HolePainter extends CustomPainter {
   final double dx;
